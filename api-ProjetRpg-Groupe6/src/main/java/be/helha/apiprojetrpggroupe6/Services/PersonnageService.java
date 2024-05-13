c7f47ccc36404ff00ee468d1e1c24af7e7a8ee68
AurelienVichi <la219485@student.helha.be>
2024-05-13 20:15:51 +0200

    Ajout getPersonnageById

--- api-ProjetRpg-Groupe6/src/main/java/be/helha/apiprojetrpggroupe6/Services/PersonnageService.java
+++ api-ProjetRpg-Groupe6/src/main/java/be/helha/apiprojetrpggroupe6/Services/PersonnageService.java
@@ -9,12 +9,14 @@ import java.util.List;
 @Service
 public class PersonnageService {
 
-    public List<Personnage>getPersonnages()
-    {
+    public List<Personnage>getPersonnages(){
         List<Personnage> list = new ArrayList<>();
         list.add(new Personnage(1, "Gandalf", 100, 1000));
         list.add(new Personnage(2, "Gimli", 50, 500));
         list.add(new Personnage(3, "Legolas", 75, 750));
         return list;
+    }
+    public Personnage getPersonnageById(int id){
+        return new Personnage(id, "Gandalf", 100, 1000);
     }
 }
--- api-ProjetRpg-Groupe6/src/main/java/be/helha/apiprojetrpggroupe6/controllers/PersonnageController.java
+++ api-ProjetRpg-Groupe6/src/main/java/be/helha/apiprojetrpggroupe6/controllers/PersonnageController.java
@@ -2,14 +2,15 @@ package be.helha.apiprojetrpggroupe6.controllers;
 
 import be.helha.apiprojetrpggroupe6.Models.Personnage;
 import be.helha.apiprojetrpggroupe6.Services.PersonnageService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class PersonnageController {
 
     private PersonnageService personnageService = new PersonnageService();
@@ -18,5 +19,10 @@ public class PersonnageController {
     public ResponseEntity<List<Personnage>> getPersonnages(){
         List<Personnage> personnages = personnageService.getPersonnages();
         return new ResponseEntity<List<Personnage>>(personnages, HttpStatus.OK);
+    }
+    @GetMapping("/personnages/{id}")
+    public ResponseEntity<Personnage> getPersonnageById(@PathVariable int id){
+        Personnage personnage = personnageService.getPersonnageById(id);
+        return new ResponseEntity<Personnage>(personnage, HttpStatus.OK);
     }
 }
