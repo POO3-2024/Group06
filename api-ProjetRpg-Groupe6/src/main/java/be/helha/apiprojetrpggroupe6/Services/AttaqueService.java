1dbb5bea52f6c434ba842ef36bef1e9642ace806
AurelienVichi <la219485@student.helha.be>
2024-05-13 21:24:40 +0200

    Mise en place attaqueController

---
+++ api-ProjetRpg-Groupe6/src/main/java/be/helha/apiprojetrpggroupe6/Services/AttaqueService.java
@@ -0,0 +1,9 @@
+package be.helha.apiprojetrpggroupe6.Services;
+
+import be.helha.apiprojetrpggroupe6.Models.Arme;
+import be.helha.apiprojetrpggroupe6.Models.Personnage;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AttaqueService {
+}
---
+++ api-ProjetRpg-Groupe6/src/main/java/be/helha/apiprojetrpggroupe6/controllers/AttaqueController.java
@@ -0,0 +1,10 @@
+package be.helha.apiprojetrpggroupe6.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AttaqueController {
+    
+}
