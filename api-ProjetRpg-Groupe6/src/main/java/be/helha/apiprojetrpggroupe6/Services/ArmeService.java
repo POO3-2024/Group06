3b43adcb004ed4432789d2a4e4081099e05eba57
AurelienVichi <la219485@student.helha.be>
2024-05-13 20:36:06 +0200

    Ajout getArmes

--- api-ProjetRpg-Groupe6/src/main/java/be/helha/apiprojetrpggroupe6/Services/ArmeService.java
+++ api-ProjetRpg-Groupe6/src/main/java/be/helha/apiprojetrpggroupe6/Services/ArmeService.java
@@ -1,7 +1,23 @@
 package be.helha.apiprojetrpggroupe6.Services;
 
+import be.helha.apiprojetrpggroupe6.Models.Arme;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ArmeService {
+
+    /**
+     * Récupère toutes les armes de la base de données
+     * @return List<Arme> Renvoi la liste des armes
+     */
+    public List<Arme> getArmes(){
+        List<Arme> list = new ArrayList<>();
+        list.add(new Arme(1, "Excalibur", 100));
+        list.add(new Arme(2, "Hache", 50));
+        list.add(new Arme(3, "Arc", 75));
+        return list;
+    }
 }
--- api-ProjetRpg-Groupe6/src/main/java/be/helha/apiprojetrpggroupe6/controllers/ArmeController.java
+++ api-ProjetRpg-Groupe6/src/main/java/be/helha/apiprojetrpggroupe6/controllers/ArmeController.java
@@ -1,12 +1,29 @@
 package be.helha.apiprojetrpggroupe6.controllers;
 
 
+import be.helha.apiprojetrpggroupe6.Models.Arme;
+import be.helha.apiprojetrpggroupe6.Services.ArmeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ArmeController {
 
+    private ArmeService armeService = new ArmeService();
 
+    /**
+     * Récupère toutes les armes de la base de données
+     * @return List<Arme> Renvoi la liste des armes
+     */
+    @GetMapping("/armes")
+    public ResponseEntity<List<Arme>> getArmes(){
+        List<Arme> armes = armeService.getArmes();
+        return new ResponseEntity<List<Arme>>(armes, HttpStatus.OK);
+    }
 }
