9b0ede13bd187aaf52df12897296d24c849c6cef
AurelienVichi <la219485@student.helha.be>
2024-05-23 16:45:51 +0200

    Adaptation personnage service et controller

--- api-ProjetRpg-Groupe6/src/main/java/be/helha/apiprojetrpggroupe6/Services/PersonnageService.java
+++ api-ProjetRpg-Groupe6/src/main/java/be/helha/apiprojetrpggroupe6/Services/PersonnageService.java
@@ -1,9 +1,12 @@
 package be.helha.apiprojetrpggroupe6.Services;
 
+import be.helha.apiprojetrpggroupe6.Database.PersonnageDatabase;
 import be.helha.apiprojetrpggroupe6.Models.DTO.PersonnageDTO;
 import be.helha.apiprojetrpggroupe6.Models.Personnage;
+import be.helha.apiprojetrpggroupe6.dbConnection.ConnectionDB;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,48 +16,48 @@ import java.util.List;
 @Service
 public class PersonnageService {
 
+    private PersonnageDatabase personnageDatabase = new PersonnageDatabase(ConnectionDB.getConnection());
     /**
      * Récupère tous les personnages de la base de données
      * Renvoi uniquement l'id et le nom des personnages
      * @return List<PersonnageDTO> Renvoi la liste des personnages
      */
-    public List<PersonnageDTO>getPersonnages(){
-        List<PersonnageDTO> list = new ArrayList<>();
-        list.add(new PersonnageDTO(1, "Gandalf"));
-        list.add(new PersonnageDTO(2, "Gimli"));
-        list.add(new PersonnageDTO(3, "Legolas"));
-        return list;
+    public List<PersonnageDTO>getPersonnages() throws SQLException {
+        return this.personnageDatabase.getAllPersonnages();
     }
     /**
      * Récupère un personnage par son id
      * @param id id du personnage
      * @return Personnage Renvoi le personnage correspondant à l'id
      */
-    public Personnage getPersonnageById(int id){
-        return new Personnage(id, "Gandalf", 100, 1000);
+    public Personnage getPersonnageById(int id) throws SQLException {
+
+        return this.personnageDatabase.getPersonnageById(id);
     }
     /**
      * Ajoute un personnage à la base de données
      * @param personnage Personnage à ajouter
      * @return Personnage Renvoi le personnage ajouté
      */
-    public Personnage addPersonnage(Personnage personnage){
-        return personnage;
+    public Personnage addPersonnage(Personnage personnage) throws SQLException {
+
+        return this.personnageDatabase.add(personnage);
     }
     /**
      * Met à jour un personnage
      * @param personnage Personnage à mettre à jour
      * @return int Renvoi le nombre de lignes modifiées
      */
-    public int updatePersonnage(Personnage personnage){
-        return 1;
+    public int updatePersonnage(Personnage personnage) throws SQLException {
+
+        return this.personnageDatabase.update(personnage);
     }
     /**
      * Supprime un personnage
      * @param id id du personnage à supprimer
      * @return int Renvoi le nombre de lignes supprimées
      */
-    public int deletePersonnage(int id){
-        return 1;
+    public int deletePersonnage(int id) throws SQLException {
+        return this.personnageDatabase.deletePersonnageById(id);
     }
 }
