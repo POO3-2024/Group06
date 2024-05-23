1f934f7fd3319840415162643fbb3d743e711310
LA228178 Lawzen <la228178@student.helha.be>
2024-05-23 09:08:27 +0200

    Merge du projet Personnage + API

--- api-ProjetRpg-Groupe6/src/main/java/be/helha/apiprojetrpggroupe6/Database/PersonnageDatabase.java
+++ api-ProjetRpg-Groupe6/src/main/java/be/helha/apiprojetrpggroupe6/Database/PersonnageDatabase.java
@@ -7,39 +7,19 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Classe CRUD pour les personnages de la base de données.
- */
 public class PersonnageDatabase {
 
-    private ConnectionDB connection;
+    private ConnectionDB connectionDB;
 
-    /**
-     * Constructeur de la classe PersonnageDatabase.
-     * Initialise la connexion à la base de données.
-     *
-     * @param connectionDB Instance de la connexion à la base de données.
-     * @throws SQLException Si une erreur survient lors de la connexion à la base de données.
-     * @throws ClassNotFoundException Si le driver de la base de données n'est pas trouvé.
-     */
     public PersonnageDatabase(ConnectionDB connectionDB) throws SQLException, ClassNotFoundException {
-        // Initialisation de la connexion à la base de données
-        connection = ConnectionDB.getConnection();
+        this.connectionDB = ConnectionDB.getConnection();
     }
 
-    /**
-     * Récupère tous les personnages de la base de données.
-     *
-     * @return Une liste de tous les personnages.
-     * @throws SQLException Si une erreur survient lors de l'exécution de la requête SQL.
-     */
     public List<Personnage> getAllPersonnages() throws SQLException {
         List<Personnage> personnages = new ArrayList<>();
-
-        // Requête pour récupérer tous les personnages
         String query = "SELECT * FROM personnage";
 
-        try (ResultSet resultSet = connection.executeQuery(query)) {
+        try (ResultSet resultSet = connectionDB.executeQuery(query)) {
             while (resultSet.next()) {
                 String nom = resultSet.getString("nom");
                 int pv = resultSet.getInt("pv");
@@ -52,27 +32,25 @@ public class PersonnageDatabase {
         return personnages;
     }
 
-    /**
-     * Ajoute un nouveau personnage dans la base de données.
-     *
-     * @param perso Le personnage à ajouter.
-     * @return Le personnage ajouté.
-     * @throws SQLException Si une erreur survient lors de l'exécution de la requête SQL.
-     */
     public Personnage add(Personnage perso) throws SQLException {
         String query = "INSERT INTO personnage (nom, pv, mana) VALUES ('" + perso.getNom() + "', " + perso.getPv() + ", " + perso.getMana() + ")";
-        connection.executeUpdate(query);
+        connectionDB.executeUpdate(query);
         return perso;
     }
 
-    /**
-     * Supprime tous les personnages de la base de données.
-     *
-     * @throws SQLException Si une erreur survient lors de l'exécution de la requête SQL.
-     */
+    public Personnage update(Personnage perso) throws SQLException {
+        String query = "UPDATE personnage SET pv = " + perso.getPv() + ", mana = " + perso.getMana() + " WHERE nom = '" + perso.getNom() + "'";
+        connectionDB.executeUpdate(query);
+        return perso;
+    }
+
+    public void delete(String nom) throws SQLException {
+        String query = "DELETE FROM personnage WHERE nom = '" + nom + "'";
+        connectionDB.executeUpdate(query);
+    }
+
     public void clearAll() throws SQLException {
-        // Requête pour supprimer tous les personnages
         String query = "DELETE FROM personnage";
-        connection.executeUpdate(query);
+        connectionDB.executeUpdate(query);
     }
 }
