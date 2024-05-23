6ba1f5d0e30c1615a3ec839d5f153adad929602b
Pusic Filip <la216187@student.helha.be>
2024-05-23 19:26:46 +0200

    javadoc des lecteurs de config json

--- projetRpg_groupe6/src/main/java/be/helha/projetrpg_groupe6/config/LectureJson.java
+++ projetRpg_groupe6/src/main/java/be/helha/projetrpg_groupe6/config/LectureJson.java
@@ -1,4 +1,4 @@
-package be.helha.projetrpg_groupe6.configNomAChanger;
+package be.helha.projetrpg_groupe6.config;
 
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
@@ -7,21 +7,41 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import java.io.FileReader;
-import java.io.IOException;
 
+/**
+ * La classe LectureJson lit le fichier de config json
+ * C'est un singleton
+ *
+ * @author Pusic Filip
+ */
 public class LectureJson {
+    /**
+     * Le chemin d'accès vers le fichier config
+     */
     static final String jsonPath = "src/main/resources/config.json";
+    /**
+     * Classe contenant les infos du fichier une fois lu
+     */
     JsonConfig jsonConfig;
+    /**
+     * Instance de LectureJson car c'est un singleton
+     */
     private static LectureJson instance;
+
+    /**
+     * Fonction pour instancier ou renvoyer l'instance de LectureJson
+     * @return l'instance si elle existe et en crée une si elle n'éxiste pas
+     */
     public static LectureJson getInstance() {
         if(instance == null) {
             instance = new LectureJson();
         }
         return instance;
     }
+
+    /**
+     * Constructeur privé de notre singleton
+     */
     private LectureJson() {
         Path path = Paths.get(jsonPath);
         Gson gson = new Gson();
@@ -31,9 +51,13 @@ public class LectureJson {
             e.printStackTrace();
         }
     }
+
+    /**
+     *
+     * @return l'url de notre fichier json
+     */
     public String getApiUrl() {
         return jsonConfig.apiUrl;
     }
-
 
 }
