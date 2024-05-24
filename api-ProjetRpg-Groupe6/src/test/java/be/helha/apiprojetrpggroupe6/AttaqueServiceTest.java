7a172f3fedc6b05cd309f23035c251e14ba8776f
AurelienVichi <la219485@student.helha.be>
2024-05-24 18:38:51 +0200

    Implémentation test attaque

---
+++ api-ProjetRpg-Groupe6/src/test/java/be/helha/apiprojetrpggroupe6/AttaqueServiceTest.java
@@ -0,0 +1,57 @@
+package be.helha.apiprojetrpggroupe6;
+
+import be.helha.apiprojetrpggroupe6.Database.ArmeDatabase;
+import be.helha.apiprojetrpggroupe6.Database.PersonnageDatabase;
+import be.helha.apiprojetrpggroupe6.Models.Arme;
+import be.helha.apiprojetrpggroupe6.Models.Personnage;
+import be.helha.apiprojetrpggroupe6.Services.ArmeService;
+import be.helha.apiprojetrpggroupe6.Services.AttaqueService;
+import be.helha.apiprojetrpggroupe6.Services.PersonnageService;
+import be.helha.apiprojetrpggroupe6.dbConnection.ConnectionDB;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class AttaqueServiceTest {
+
+    private AttaqueService attaqueService = new AttaqueService(true);
+    private PersonnageService personnageService = new PersonnageService(true);
+    private ArmeService armeService = new ArmeService(true);
+    private Personnage perso = new Personnage("Test",100,30);
+    private Arme arme = new Arme("Baton", 10);
+    private Arme arme2 = new Arme("Epee", 50);
+
+
+    @BeforeEach
+    public void init() throws SQLException {
+        try {
+            this.perso = personnageService.addPersonnage(perso);
+            this.arme = armeService.addArme(arme);
+            this.arme2 = armeService.addArme(arme2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterEach
+    public void clean() {
+        try {
+            personnageService.deletePersonnage(perso.getId());
+            armeService.deleteArme(arme.getId());
+            armeService.deleteArme(arme2.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @AfterAll
+    public static void cleanAll(){
+        ConnectionDB.getConnection(true).closeConnection();
+    }
+}
