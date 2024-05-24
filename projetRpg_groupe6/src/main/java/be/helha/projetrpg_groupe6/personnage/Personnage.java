7437b5363cad9aede3e33eb3ef1b21a8565e8dea
Pusic Filip <la216187@student.helha.be>
2024-05-24 18:40:13 +0200

    javadoc de personnage

--- projetRpg_groupe6/src/main/java/be/helha/projetrpg_groupe6/personnage/Personnage.java
+++ projetRpg_groupe6/src/main/java/be/helha/projetrpg_groupe6/personnage/Personnage.java
@@ -4,6 +4,9 @@ import be.helha.projetrpg_groupe6.services.PersonnageService;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * Classe personnage
+ */
 public class Personnage {
 
     @SerializedName("id")
@@ -22,11 +25,22 @@ public class Personnage {
     @Expose
     private int mana;
 
+    /**
+     * Constructeur personnage avec les attributs
+     * @param nom
+     * @param pv
+     * @param mana
+     */
     public Personnage(String nom, int pv, int mana) {
         this.nom = nom;
         this.pv = pv;
         this.mana = mana;
     }
+
+    /**
+     * Constructeur à partir d'un autre personnage
+     * @param personnage
+     */
     public Personnage(Personnage personnage){
         this.id = personnage.getId();
         this.nom = personnage.getNom();
@@ -34,40 +48,71 @@ public class Personnage {
         this.mana = personnage.getMana();
     }
 
+    /**
+     * Constructeur de personnage de base
+     * @param nom
+     */
     public Personnage(String nom) {
         this.nom = nom;
         this.pv = 10;
         this.mana = 10;
     }
 
+    /**
+     * Fonction getId
+     * @return l'id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Fonction pour setter l'id
+     * @param id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Fonction getNom
+     * @return le nom
+     */
     public String getNom() {
         return nom;
     }
-
+    /**
+     * Fonction pour définir le nom
+     * @param nom
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
-
+    /**
+     * Fonction getPv
+     * @return les pv
+     */
     public int getPv() {
         return pv;
     }
-
+    /**
+     * Fonction pour définir des pv
+     * @param pv
+     */
     public void setPv(int pv) {
         this.pv = pv;
     }
-
+    /**
+     * Fonction pour renvoyer le mana
+     * @return le mana en question
+     */
     public int getMana() {
         return mana;
     }
-
+    /**
+     * Fonction pour définir le mana
+     * @param mana
+     */
     public void setMana(int mana) {
         this.mana = mana;
     }
